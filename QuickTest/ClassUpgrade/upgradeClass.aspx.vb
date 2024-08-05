9277fcc982954612589dd7f1d1b85e7ca8d5bd09
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:12:25 +0700

    Create upgradeClass.aspx.vb

---
+++ QuickTest/ClassUpgrade/upgradeClass.aspx.vb
@@ -0,0 +1,16 @@
+﻿Public Class upgradeClass
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        '28/01/2014 ปิดไว้ก่อน หน้านี้ยังไม่ได้ทดสอบ ให้ทดสอบก่อนใช้ใหม่
+
+        If Session("UserId") = Nothing Then
+            Response.Redirect("~/LoginPage.aspx", False)
+        End If
+    End Sub
+
+    Protected Sub btnUpgradeClass_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnUpgradeClass.Click
+        Response.Redirect("../ClassUpgrade/changeClass.aspx")
+    End Sub
+End Class
