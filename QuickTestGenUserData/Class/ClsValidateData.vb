be3f3db3a1b6fa7440bd2dd1a31bf2f555fc7dd1
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 11:28:50 +0700

    Create ClsValidateData.vb

---
+++ QuickTestGenUserData/Class/ClsValidateData.vb
@@ -0,0 +1,13 @@
+﻿Public Class ClsValidateData
+    Public Function OrderbyDatatable(dt As DataTable, ColumnIdName As String) As DataTable
+        Dim Orderdt As DataTable
+        Dim dataView As New DataView(dt)
+
+        dataView.Sort = ColumnIdName & " ASC"
+
+        Orderdt = dataView.ToTable()
+
+        Return Orderdt
+    End Function
+
+End Class
