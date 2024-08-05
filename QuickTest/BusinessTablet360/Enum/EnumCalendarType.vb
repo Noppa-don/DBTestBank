3dbcfb4f5e17e0ce253d493c8e4800648524683d
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:06:43 +0700

    Create EnumCalendarType.vb

---
+++ QuickTest/BusinessTablet360/Enum/EnumCalendarType.vb
@@ -0,0 +1,17 @@
+﻿
+Public Enum EnCalendarType
+    Normal = 1
+    Holiday = 2
+    Term = 3
+End Enum
+
+Public Class EnumCalendarType
+    Inherits EnumRegister
+
+    Public Sub New()
+        AddItem(EnCalendarType.Normal, "ทั่วไป")
+        AddItem(EnCalendarType.Holiday, "วันหยุดนักขัตฤกษ์")
+        AddItem(EnCalendarType.Term, "เทอมการศึกษา")
+    End Sub
+
+End Class
