c7eb790b905d0729ca8551343a5a899c0b3b70d7
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:13:43 +0700

    Create MsgList.vb

---
+++ QuickTest/Common/MsgList.vb
@@ -0,0 +1,10 @@
+﻿Public Class MsgList
+
+    Public Shared SaveComplete As String = "บันทึกข้อมูลเรียบร้อยแล้วค่ะ"
+    Public Shared SaveNotComplete As String = "ไม่สามารถบันทึกข้อมูลได้"
+    Public Shared EditComplete As String = "แก้ไขข้อมูลเรียบร้อยแล้วค่ะ"
+    Public Shared DeleteComplete As String = "ลบข้อมูลเรียบร้อยแล้ว"
+    Public Shared DeleteNotComplete As String = "ไม่สามารถลบข้อมูลได้"
+    Public Shared PleaseInput As String = "กรุณาระบุ"
+
+End Class
