c8f60d664398700b9de7b15195444a71b7e167ca
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 14:44:44 +0700

    Create ShowHowToPage.aspx.vb

---
+++ QuickTest/ShowHowToPage.aspx.vb
@@ -0,0 +1,23 @@
+﻿Public Class ShowHowToPage
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        'HowTo.InnerHtml = "<iframe src=""../HowTo/HowToSelectClassSubject/HowToSelectClassSubject.htm"" width='100%' height='500'></iframe>"
+    End Sub
+
+    '<Services.WebMethod()>
+    'Public Shared Function getIframeTutorial(ByVal tutorial As String) As String
+    '    If (tutorial = "introduction") Then
+    '        'Return getIframeTutorial = "<iframe src=""../HowTo/HowToSelectClassSubject/HowToSelectClassSubject.htm"" width='100%' height='500'></iframe>"
+    '        getIframeTutorial = tutorial
+    '    ElseIf (tutorial = "manageQuiz") Then
+    '        'Return getIframeTutorial = "<iframe src=""../HowTo/HowToSelectClassSubject/HowToSelectClassSubject.htm"" width='100%' height='500'></iframe>"
+    '        getIframeTutorial = tutorial
+    '    ElseIf (tutorial = "modifyQuiz") Then
+    '        'Return getIframeTutorial = "<iframe src=""../HowTo/HowToSelectClassSubject/HowToSelectClassSubject.htm"" width='100%' height='500'></iframe>"
+    '        getIframeTutorial = tutorial
+    '    End If
+    '    Return getIframeTutorial
+    'End Function
+
+End Class
