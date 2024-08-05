46e4ed9155669cf7b5da1c5227ccf3c081bfe8fc
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 14:41:39 +0700

    Create Scheduler.aspx.vb

---
+++ QuickTest/Scheduler/Scheduler.aspx.vb
@@ -0,0 +1,26 @@
+﻿Public Class Scheduler
+
+    Inherits System.Web.UI.Page
+    Dim ClsScheduler As New ClsScheduler(New ClassConnectSql)
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Try
+            If HttpContext.Current.Application("NeedEditButton") Is Nothing Then
+                Try
+                    KNConfigData.LoadData()
+                Catch ex As Exception When Not TypeOf ex Is Threading.ThreadAbortException
+                    Elmah.ErrorSignal.FromCurrentContext().Raise(ex)
+                End Try
+
+            End If
+
+            ClsScheduler.NextSemester(HttpContext.Current.Application("DefaultSchoolCode"))
+
+        Catch ex As Exception When Not TypeOf ex Is Threading.ThreadAbortException
+            Elmah.ErrorSignal.FromCurrentContext().Raise(ex)
+        End Try
+
+    End Sub
+
+End Class
