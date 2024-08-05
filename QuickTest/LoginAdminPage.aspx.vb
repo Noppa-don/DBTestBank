e8ecf663ee48cc4c9a25f16c11c5a84c5fbf4bcb
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 14:27:58 +0700

    Create LoginAdminPage.aspx.vb

---
+++ QuickTest/LoginAdminPage.aspx.vb
@@ -0,0 +1,9 @@
+﻿Public Class LoginAdminPage
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Session("FromLoginAdminPage") = "1"
+        Response.Redirect("loginpage.aspx", False)
+    End Sub
+
+End Class
