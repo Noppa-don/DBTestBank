3a20d16c10e114e6fb3fea12ef2497fb86389882
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 14:38:05 +0700

    Create GenQRStudent.aspx.vb

---
+++ QuickTest/QRReport/GenQRStudent.aspx.vb
@@ -0,0 +1,15 @@
+﻿Public Class GenQRStudent
+    Inherits System.Web.UI.Page
+    Public StudentId As String
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        If HttpContext.Current.Request.QueryString("StudentId") IsNot Nothing And HttpContext.Current.Request.QueryString("StudentId").ToString() <> "" Then
+            StudentId = HttpContext.Current.Request.QueryString("StudentId").ToString()
+        Else
+            Response.Write("ไม่มี StudentId")
+        End If
+
+    End Sub
+
+End Class
