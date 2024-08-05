923214f1f4eecd6e4d38f2349d147e303de3786e
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:11:55 +0700

    Create AlternativeClassUpgrade.aspx.vb

---
+++ QuickTest/ClassUpgrade/AlternativeClassUpgrade.aspx.vb
@@ -0,0 +1,12 @@
+﻿Public Class AlternativeClassUpgrade
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        '28/01/2014 ปิดไว้ก่อน หน้านี้ยังไม่ได้ทดสอบ ให้ทดสอบก่อนใช้ใหม่
+        If Session("UserId") = Nothing Then
+            Response.Redirect("~/LoginPage.aspx", False)
+        End If
+    End Sub
+
+End Class
