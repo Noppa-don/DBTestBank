afca99fc7ede5698c6c663b5bff66ca1b2541344
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:07:06 +0700

    Create EnumModuleStatus.vb

---
+++ QuickTest/BusinessTablet360/Enum/EnumModuleStatus.vb
@@ -0,0 +1,18 @@
+﻿Public Enum EnModuleStatus
+    NotDone
+    NotFinish
+    FinishNotComplete
+    FinishComplete
+End Enum
+
+Public Class EnumModuleStatus
+    Inherits EnumRegister
+
+    Public Sub New()
+        AddItem(EnModuleStatus.NotDone, "ไม่ทำ")
+        AddItem(EnModuleStatus.NotFinish, "ทำไม่เสร็จ")
+        AddItem(EnModuleStatus.FinishNotComplete, "ทำเสร็จไม่ครบ")
+        AddItem(EnModuleStatus.FinishComplete, "ทำเสร็จครบ")
+    End Sub
+
+End Class
