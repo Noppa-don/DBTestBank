9c5378b730190a2104bdcd9268942ddd663af893
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 14:08:38 +0700

    Create LayoutConfirmed.js

---
+++ QuickTest/js/LayoutConfirmed.js
@@ -0,0 +1,25 @@
+﻿function UpdateLayoutConfirmed(updateField, tick, questionId) {
+    $.ajax({
+        type: "POST",
+        async: false,
+        url: ResolveURL + 'WebServices/TestsetService.asmx/UpdateLayoutConfirmed',
+        data: "{ UpdateField : '" + updateField + "', Tick : '" + tick + "', QuestionId : '" + questionId + "'}",  //" 
+    contentType: "application/json; charset=utf-8", dataType: "json",
+    success: function (msg) {
+    }
+});
+}
+
+function SaveLog(LogText) {
+    alert(1);
+    $.ajax({
+        type: "POST",
+        async: false,
+        url: ResolveURL + 'WebServices/TestsetService.asmx/SaveLog',
+        data: "{ LogText : '" + LogText + "'}",  //" 
+        contentType: "application/json; charset=utf-8", dataType: "json",
+        success: function (msg) {
+        }
+    });
+}
+
