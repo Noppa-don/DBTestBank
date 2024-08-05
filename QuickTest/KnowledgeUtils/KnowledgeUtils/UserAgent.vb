8a601b01b9985a4ce864f223325cc9f3634614a2
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 14:17:15 +0700

    Create UserAgent.vb

---
+++ QuickTest/KnowledgeUtils/KnowledgeUtils/UserAgent.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Web
+
+Public Module UserAgent
+
+    Public Function IsAndroid() As Boolean
+        Dim AgentString As String = HttpContext.Current.Request.UserAgent.ToString()
+        If AgentString.ToLower().IndexOf("android") <> -1 Then
+            Return True
+        End If
+        Return False
+    End Function
+End Module
