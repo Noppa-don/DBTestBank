2d569d0c4170a8ad49556a250f764e43b06c339e
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:07:49 +0700

    Create EnumTabletOwnerType.vb

---
+++ QuickTest/BusinessTablet360/Enum/EnumTabletOwnerType.vb
@@ -0,0 +1,18 @@
+﻿Public Enum EnTabletOwnerType
+    Teacher = 1
+    Student = 2
+    Lab = 3
+    Spare = 4
+End Enum
+
+Public Class EnumTabletOwnerType
+    Inherits EnumRegister
+
+    Public Sub New()
+        AddItem(EnTabletOwnerType.Teacher, "ครู")
+        AddItem(EnTabletOwnerType.Student, "นักเรียน")
+        AddItem(EnTabletOwnerType.Lab, "เครื่องในห้องแล็บ")
+        AddItem(EnTabletOwnerType.Spare, "เครื่องสำรองส่วนกลาง")
+    End Sub
+
+End Class
