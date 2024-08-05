ee44cad39344c538100c608c7e81309606760935
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:07:42 +0700

    Create EnumStudentProcessType.vb

---
+++ QuickTest/BusinessTablet360/Enum/EnumStudentProcessType.vb
@@ -0,0 +1,13 @@
+﻿Public Enum EnStudentProcessType
+    Up
+    Finish
+End Enum
+
+Public Class EnumStudentProcessType
+    Inherits EnumRegister
+
+    Public Sub New()
+        AddItem(EnStudentProcessType.Up, "เลื่อนชั้น")
+        AddItem(EnStudentProcessType.Finish, "จบ")
+    End Sub
+End Class
