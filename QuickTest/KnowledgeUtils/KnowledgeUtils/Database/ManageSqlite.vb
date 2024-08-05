46dc0df0eee6e561cd73e865e2958fe7ed5bde6a
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 14:13:48 +0700

    Create ManageSqlite.vb

---
+++ QuickTest/KnowledgeUtils/KnowledgeUtils/Database/ManageSqlite.vb
@@ -0,0 +1,20 @@
+﻿
+Namespace Database.Sqlite
+
+    ''' <summary>
+    ''' คลาสช่วยจัดการ Sqlite
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Class ManageSqlite
+
+        Public Shared Function ConvertDateTime(DateSource As DateTime) As String
+            Dim D = DateSource.Year.ToString & "-" & DateSource.Month.ToString("00") & "-" & DateSource.Day.ToString("00")
+            Dim T = DateSource.Hour.ToString("00") & ":" & DateSource.Minute.ToString("00") & ":" & DateSource.Second.ToString("00")
+            Return D & " " & T
+        End Function
+
+    End Class
+
+End Namespace
+
+
