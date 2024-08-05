8677762e7d90ff2fa57b3134f7e63fc069e22e09
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 11:29:58 +0700

    Create DialogTwoInput.aspx.vb

---
+++ QuickTestGenUserData/Common/DialogTwoInput.aspx.vb
@@ -0,0 +1,18 @@
+﻿Public Class DialogTwoInput
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim Msg() = Request.QueryString("msg").Split(",")
+        Dim Title = Msg(0)
+        Dim Head1 = Msg(1)
+        Dim OldValue1 = Msg(2)
+        Dim Head2 = Msg(3)
+        Dim OldValue2 = Msg(4)
+        Me.Title = Msg(0)
+        lblHead1.Text = Head1
+        TxtData1.Text = OldValue1
+        lblHead2.Text = Head2
+        TxtData2.Text = OldValue2
+    End Sub
+
+End Class
