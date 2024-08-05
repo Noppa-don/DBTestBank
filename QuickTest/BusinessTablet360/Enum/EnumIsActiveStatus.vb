91707ab0683a0845f742391d70c0e7f51b48e849
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:06:56 +0700

    Create EnumIsActiveStatus.vb

---
+++ QuickTest/BusinessTablet360/Enum/EnumIsActiveStatus.vb
@@ -0,0 +1,14 @@
+﻿Public Enum EnIsActiveStatus
+    Delete
+    Active
+End Enum
+
+Public Class EnumIsActiveStatus
+    Inherits EnumRegister
+
+    Public Sub New()
+        AddItem(EnIsActiveStatus.Delete, "ข้อมูลถูกลบ")
+        AddItem(EnIsActiveStatus.Active, "ข้อมูลยังใช้อยู่")
+    End Sub
+
+End Class
