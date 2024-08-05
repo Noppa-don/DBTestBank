1a97ab89b9cb9d035842deb6d17336374b31ac66
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:07:12 +0700

    Create EnumNetworkType.vb

---
+++ QuickTest/BusinessTablet360/Enum/EnumNetworkType.vb
@@ -0,0 +1,20 @@
+﻿Public Enum EnNetworkType
+    AccessPoint = 1
+    Server = 2
+    Switch = 3
+    Router = 4
+    OTH = 10
+End Enum
+
+Public Class EnumNetworkType
+    Inherits EnumRegister
+
+    Public Sub New()
+        AddItem(EnNetworkType.AccessPoint, "AP-AccessPoint")
+        AddItem(EnNetworkType.Server, "SVR-Server")
+        AddItem(EnNetworkType.Switch, "SW")
+        AddItem(EnNetworkType.Router, "RT-Router")
+        AddItem(EnNetworkType.OTH, "OTH-อื่นๆ")
+    End Sub
+
+End Class
