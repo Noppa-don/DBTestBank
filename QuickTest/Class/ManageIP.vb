2d6c46e9b1262a80a1b53bb7416bff76113819ad
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:11:12 +0700

    Create ManageIP.vb

---
+++ QuickTest/Class/ManageIP.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Net
+
+Public Class ManageIP
+
+    Public Shared Sub SetNewIP(ByVal UserNameWithAdminRight As String, ByVal UserPassword As String, ByVal UserDomain As String, ByVal NewIP As IPAddress, _
+                               ByVal NewGateWay As IPAddress, ByVal NewSubnetMask As IPAddress, ByVal NewDNS1 As IPAddress, ByVal NewDNS2 As IPAddress, ByVal LanInterfaceName As String)
+
+
+    End Sub
+
+End Class
