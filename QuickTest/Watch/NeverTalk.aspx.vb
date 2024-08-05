4b37b23439f105221bf762785b15062f5041e604
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 15:06:56 +0700

    Create NeverTalk.aspx.vb

---
+++ QuickTest/Watch/NeverTalk.aspx.vb
@@ -0,0 +1,11 @@
+﻿Public Class NeverTalk
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Private Sub btnRefresh_Click(sender As Object, e As EventArgs) Handles btnRefresh.Click
+        Response.Redirect("~/Watch/ChatSelectStudent.aspx?DeviceId=" & Request.QueryString("DeviceId").ToString())
+    End Sub
+End Class
