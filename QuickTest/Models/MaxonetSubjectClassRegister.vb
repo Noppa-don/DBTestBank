a54bf659cea453b346264bc83879e5bd71159f50
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 14:33:45 +0700

    Create MaxonetSubjectClassRegister.vb

---
+++ QuickTest/Models/MaxonetSubjectClassRegister.vb
@@ -0,0 +1,7 @@
+﻿Public Class MaxonetSubjectClassRegister
+    Property SubjectId As String
+    Property ClassId As New List(Of String)
+    Property Registered As New List(Of Integer)
+
+End Class
+
