ed5854e6df4cf669edf402e6adedbdfd62f85c31
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:10:26 +0700

    Create ClsSecurity.vb

---
+++ QuickTest/Class/ClsSecurity.vb
@@ -0,0 +1,9 @@
+﻿Public Class ClsSecurity
+    Public Shared Sub CheckConnectionIsSecure()
+#If F5 <> "1" Then
+        'If HttpContext.Current.Request.IsSecureConnection() = False Then
+        '    Throw New Exception("connection is insecurity")
+        'End If
+#End If
+    End Sub
+End Class
