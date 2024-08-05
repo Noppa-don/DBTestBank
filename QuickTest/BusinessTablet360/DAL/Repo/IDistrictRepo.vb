f9079bb5763674b9349d76c3c01ab5ccfc75fd5a
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:06:03 +0700

    Create IDistrictRepo.vb

---
+++ QuickTest/BusinessTablet360/DAL/Repo/IDistrictRepo.vb
@@ -0,0 +1,15 @@
+﻿Imports KnowledgeUtils.Database.DBFactory.LinqToSql(Of BusinessTablet360.DataClassesTablet360DataContext)
+Imports KnowledgeUtils.Database.DBFactory
+Imports KnowledgeUtils.Database
+
+Public Interface IDistrictRepo
+    Function GetDistrictByCrit(ByVal Ctx As DataClassesTablet360DataContext, ByVal Item As tblProvince) As tblAmphur()
+End Interface
+
+Public Class DistrictRepo
+    Implements IDistrictRepo
+
+    Public Function GetDistrictByCrit(ByVal Ctx As DataClassesTablet360DataContext, ByVal Item As tblProvince) As tblAmphur() Implements IDistrictRepo.GetDistrictByCrit
+        Return Ctx.tblAmphurs.Where(Function(r) r.ProvinceId = Item.ProvinceId).ToArray
+    End Function
+End Class
