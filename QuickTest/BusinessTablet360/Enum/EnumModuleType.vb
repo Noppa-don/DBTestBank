b41ae3142da8bc8a37cca3c4a23854a7f87a8500
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:07:09 +0700

    Create EnumModuleType.vb

---
+++ QuickTest/BusinessTablet360/Enum/EnumModuleType.vb
@@ -0,0 +1,31 @@
+﻿Public Enum EnModuleType
+    Homework
+End Enum
+
+Public Class EnumModuleType
+    Inherits EnumRegister
+
+    Public Sub New()
+        AddItem(EnModuleType.Homework, "การบ้าน")
+    End Sub
+End Class
+
+Public Enum EnModuleUI
+    MdModule
+    MdClass
+    MdRoom
+    MdStudent
+    MdModuleHomeworkTime
+End Enum
+
+Public Class EnumModuleUI
+    Inherits EnumRegister
+
+    Public Sub New()
+        AddItem(EnModuleUI.MdModule, "MdModule")
+        AddItem(EnModuleUI.MdClass, "MdClass")
+        AddItem(EnModuleUI.MdRoom, "MdRoom")
+        AddItem(EnModuleUI.MdStudent, "MdStudent")
+        AddItem(EnModuleUI.MdModuleHomeworkTime, "MdModuleHomeworkTime")
+    End Sub
+End Class
