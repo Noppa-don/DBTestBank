390eb5928984f457da164ec8064f10899f963d1c
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:03:22 +0700

    Create UserDTO.vb

---
+++ QuickTest/BusinessTablet360/BLL/DTO/UserDTO.vb
@@ -0,0 +1,13 @@
+﻿Public Class UserDTO
+
+    Public Property School_Code As String
+    Public Property User_Id As Guid?
+    Public Property User_Name As String
+    Public Property User_IsActive As Boolean?
+    Public Property MenuItem_Code As Short?
+
+    Public Sub New()
+        User_IsActive = True
+    End Sub
+
+End Class
