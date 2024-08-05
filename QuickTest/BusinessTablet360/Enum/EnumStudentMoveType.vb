9d0a6cbb9d37fdc8d0a40a6a6361b42fb5870254
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:07:39 +0700

    Create EnumStudentMoveType.vb

---
+++ QuickTest/BusinessTablet360/Enum/EnumStudentMoveType.vb
@@ -0,0 +1,18 @@
+﻿Public Enum EnStudentMoveType
+    Normal = 1 'ปกติรวมไปถึงเข้าใหม่ และ เลื่อนชั้นด้วย
+    MoveRoom = 2 'ย้ายห้อง
+
+    EditClassUp = 6 'แบบนี้เราจะไม่ได้เอาสาถานะเก็บเข้าดาต้าเบสนะ แค่ไว้ดักเช็คตอนส่งจาก ui เข้า bll
+    EditClassDown = 7 'แบบนี้เราจะไม่ได้เอาสาถานะเก็บเข้าดาต้าเบสนะ แค่ไว้ดักเช็คตอนส่งจาก ui เข้า bll
+    EditRoom = 8 'แก้ไขจากหน้า แก้ไขนักเรียน
+End Enum
+
+Public Class EnumStudentMoveType
+    Inherits EnumRegister
+
+    Public Sub New()
+        AddItem(EnStudentMoveType.Normal, "ปกติ")
+        AddItem(EnStudentMoveType.MoveRoom, "ย้ายห้อง")
+    End Sub
+
+End Class
