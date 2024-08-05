49c926cc285517ff09afdd8b3231250b53fa4fc4
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:23:16 +0700

    Create NotUsage.aspx.vb

---
+++ QuickTest/DroidPad/NotUsage.aspx.vb
@@ -0,0 +1,14 @@
+﻿Public Class NotUsage
+    Inherits System.Web.UI.Page
+
+    Protected IsMaxOnet As Boolean
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        ' ดักไว้ ถ้า Application ทั้งหมด Is Nothing ให้โหลดค่าขึ้นมาใหม่ กรณีนี้เจอตอน ฝึกฝนจากคอมพิวเตอร์
+        If HttpContext.Current.Application("NeedEditButton") Is Nothing Then
+            KNConfigData.LoadData()
+        End If
+
+        IsMaxOnet = ClsKNSession.IsMaxONet
+    End Sub
+
+End Class
