6c65eabcc4012db434cedc8a2457512c55a39ca9
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 15:02:32 +0700

    Create SelectTermControl.ascx.vb

---
+++ QuickTest/UserControl/SelectTermControl.ascx.vb
@@ -0,0 +1,10 @@
+﻿Public Class SelectTermControl
+    Inherits System.Web.UI.UserControl
+    Dim KnSession As New KNAppSession()
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim UserID As String = Session("UserId").ToString()
+        lblTerm.InnerHtml = KnSession.StoredValue("SelectedCalendarName").ToString()
+    End Sub
+
+End Class
