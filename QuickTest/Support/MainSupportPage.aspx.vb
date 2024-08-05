f255da04baa0b2b078f83c1b58e66cca96bf3ded
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 14:51:22 +0700

    Create MainSupportPage.aspx.vb

---
+++ QuickTest/Support/MainSupportPage.aspx.vb
@@ -0,0 +1,14 @@
+﻿Public Class MainSupportPage
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Not Page.IsPostBack Then
+            If ClassSupport.CheckInternetConnection() = True Then
+                Response.Redirect("~/Support/PostErrorDetails.aspx")
+            Else
+                Response.Redirect("~/Support/ShowContactInfo.aspx")
+            End If
+        End If
+    End Sub
+
+End Class
