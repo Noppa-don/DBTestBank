eb7853dd7abf80b16ac9c3de54ff86f540a6c8d7
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:24:25 +0700

    Create test.aspx.vb

---
+++ QuickTest/DroidPad/test.aspx.vb
@@ -0,0 +1,32 @@
+﻿Public Class test
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        GenSI()
+    End Sub
+    Public Shared Function GenSI()
+        Dim db As New ClassConnectSql()
+        Dim sql As String
+        Dim dt As DataTable
+
+        sql = "SELECT guid from tblschool where isactive  = 1 "
+        dt = db.getdata(sql)
+
+
+        Dim execQ As String = ""
+        If dt.Rows.Count > 0 Then
+            For Each row As DataRow In dt.Rows
+                'JsonString = New With {.stuId = row("Student_Id")}
+                execQ = "insert into tblschoolinfo (si_id, school_guid,  si_password, isactive, lastupdate) values ("
+                execQ = execQ + "newid(), '" + row("guid").ToString() + "', '" + Encryption.MD5("1234") + "', '1', dbo.GetThaiDate());"
+                '// ปิดไว้ก่อน เพราะใช้ได้จริง เดี๋ญวข้อมูลรวน                 
+                '//db.Execute(execQ)
+            Next
+        End If
+ 
+    End Function
+End Class
