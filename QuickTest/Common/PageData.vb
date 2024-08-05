4f7df8adf4857eccb23be480d00350dbbc317af0
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:13:47 +0700

    Create PageData.vb

---
+++ QuickTest/Common/PageData.vb
@@ -0,0 +1,7 @@
+﻿Public Class PageData
+    Public Property Data1 As String
+    Public Property Data2 As String
+    Public Property Data3 As String
+    Public Property Data4 As String
+    Public Property Data5 As String
+End Class
