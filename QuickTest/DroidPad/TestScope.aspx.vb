f95717dc069bfa0f3fd1b4b38111eb425c2e7fe2
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:24:44 +0700

    Create TestScope.aspx.vb

---
+++ QuickTest/DroidPad/TestScope.aspx.vb
@@ -0,0 +1,10 @@
+﻿Public Class TestScope
+    Inherits System.Web.UI.Page
+    Dim UseCls As New ClassConnectSql
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Session("AllowRegisteredTabletOwnerToLogIn") = "True"
+        'Session("_SchoolID") = "1000008"
+    End Sub
+
+End Class
