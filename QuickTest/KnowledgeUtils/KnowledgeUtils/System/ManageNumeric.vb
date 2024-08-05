b78b67b3c1b1cba3174bb7f5610dee7fec45708c
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 14:17:02 +0700

    Create ManageNumeric.vb

---
+++ QuickTest/KnowledgeUtils/KnowledgeUtils/System/ManageNumeric.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Runtime.CompilerServices
+
+Namespace System.Numeric
+
+    Public Module ModuleManageNumeric
+
+        ''' <summary>
+        ''' ฟังชั่น + ค่าตัวแปรตามจำนวนเลขที่ส่งเข้ามา
+        ''' </summary>
+        ''' <param name="Source">เลข integer ที่ต้องการจะบวก</param>
+        ''' <param name="Plus">จำนวนค่าที่จะบวก</param>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        <Extension()>
+        Public Function IntegerPlus(ByRef Source As Integer, ByVal Plus As Integer) As Integer
+            Source = Source + Plus
+            Return Source
+        End Function
+
+    End Module
+
+End Namespace
+
