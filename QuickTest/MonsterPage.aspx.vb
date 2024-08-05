b382bac0315c0bf1403628b36559b42fe35cc118
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 14:34:28 +0700

    Create MonsterPage.aspx.vb

---
+++ QuickTest/MonsterPage.aspx.vb
@@ -0,0 +1,9 @@
+﻿Public Class MonsterPage
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim m As New MonsterIDTemp
+        m.CreateMonsterTest()
+    End Sub
+
+End Class
