c01c7d96f1bfad22fa8f5533ad8f6f65e6182705
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:04:51 +0700

    Create MD5.vb

---
+++ QuickTest/BusinessTablet360/Class/MD5.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Security.Cryptography
+Imports System.Text
+Public Class Encryption
+
+    Public Shared Function MD5(ByVal strPlainText As String) As String
+        Dim ASCIIenc As New ASCIIEncoding
+        Dim strReturn As String
+        Dim ByteSourceText() As Byte = ASCIIenc.GetBytes(strPlainText)
+        Dim Md5Hash As New MD5CryptoServiceProvider
+        Dim ByteHash() As Byte = Md5Hash.ComputeHash(ByteSourceText)
+
+        strReturn = ""
+
+        For Each b As Byte In ByteHash
+            strReturn = strReturn & b.ToString("x2")
+        Next
+        Return strReturn
+    End Function
+End Class
