3138b8fed86f55c75d537c5247e803d0cd29af57
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 14:55:59 +0700

    Create MoveExamPage.aspx.vb

---
+++ QuickTest/TestSet/MoveExamPage.aspx.vb
@@ -0,0 +1,21 @@
+﻿Public Class MoveExamPage
+    Inherits System.Web.UI.Page
+    Protected Shared clslayoutcheck As New ClsLayoutCheckConfirmed()
+
+    'เก็บ QsetId เอาไว้ใช้กับหน้า Javascript ด้วย
+    Public Property Qset_Id As String
+        Get
+            Return ViewState("_Qset_Id")
+        End Get
+        Set(ByVal value As String)
+            ViewState("_Qset_Id") = value
+        End Set
+    End Property
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Request.QueryString("qsetid") IsNot Nothing Then
+            Qset_Id = Request.QueryString("qsetid").ToString()
+            clslayoutcheck.BindRepeater(Qset_Id, "Quiz", rptListQuestion, True)
+        End If
+    End Sub
+
+End Class
