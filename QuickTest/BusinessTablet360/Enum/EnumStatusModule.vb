b69cf2ab04aad6a597d7e0f4408efa68724cb136
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:07:32 +0700

    Create EnumStatusModule.vb

---
+++ QuickTest/BusinessTablet360/Enum/EnumStatusModule.vb
@@ -0,0 +1,17 @@
+﻿Public Enum EnStatusModule
+    HomeworkNotStart
+    HomeworkNotFinish
+    HomeworkFinish
+End Enum
+
+Public Class EnumStatusModule
+    Inherits EnumRegister
+
+    Public Sub New()
+        AddItem(EnStatusModule.HomeworkNotStart, "ยังไม่ทำการบ้าน")
+        AddItem(EnStatusModule.HomeworkNotFinish, "ยังทำการบ้านไม่เสร็จ")
+        AddItem(EnStatusModule.HomeworkFinish, "ทำการบ้านเสร็จแล้ว")
+
+    End Sub
+
+End Class
