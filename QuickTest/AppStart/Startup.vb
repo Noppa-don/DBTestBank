9bcf633ade3dc711145aa8299ce4f8aaf1e8cdd8
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 11:45:32 +0700

    Create Startup.vb

---
+++ QuickTest/AppStart/Startup.vb
@@ -0,0 +1,21 @@
+﻿Imports Owin
+Imports System
+Imports Microsoft.Owin
+
+
+
+<Assembly: OwinStartup(GetType(SignalRChat2.Startup))>
+Namespace SignalRChat2
+
+    Public Class Startup
+        Public Sub Configuration(app As IAppBuilder)
+            app.MapSignalR()
+
+        End Sub
+    End Class
+End Namespace
+
+
+
+
+
