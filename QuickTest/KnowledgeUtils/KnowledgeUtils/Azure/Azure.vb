4e6ddec5aceff694765c98dc7df29c809231022b
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 14:10:30 +0700

    Create Azure.vb

---
+++ QuickTest/KnowledgeUtils/KnowledgeUtils/Azure/Azure.vb
@@ -0,0 +1,8 @@
+﻿Public Class Azure
+
+    'ถ้า run in Azure จะ มี ตัวแปร ชื่อ WEBSITE_SITE_NAME นี้
+    Public Shared Function IsRunInAzure() As Boolean
+        Return Not [String].IsNullOrEmpty(Environment.GetEnvironmentVariable("WEBSITE_SITE_NAME"))
+    End Function
+
+End Class
