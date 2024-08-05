4d73705fd12e569db695ad63d8c2ca1383006acc
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:02:46 +0700

    Create MenuItemDTO.vb

---
+++ QuickTest/BusinessTablet360/BLL/DTO/MenuItemDTO.vb
@@ -0,0 +1,12 @@
+﻿Public Class MenuItemDTO
+
+    Public Property MenuItem_Code As String
+    Public Property MenuItem_Parent As String
+    Public Property MenuItem_Type As Byte?
+    Public Property MenuItem_IsActive As Boolean?
+
+    Public Sub New()
+        MenuItem_IsActive = True
+    End Sub
+
+End Class
