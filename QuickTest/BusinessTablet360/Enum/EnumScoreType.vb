565b86fc972714c29cdd68ec02ebf6f0142197de
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:07:29 +0700

    Create EnumScoreType.vb

---
+++ QuickTest/BusinessTablet360/Enum/EnumScoreType.vb
@@ -0,0 +1,13 @@
+﻿Public Enum enScoreType
+    TestInClass = 1
+    Homework = 2
+End Enum
+
+Public Class EnumScoreType
+    Inherits EnumRegister
+
+    Public Sub New()
+        AddItem(enScoreType.TestInClass, "ผลทดสอบในห้อง")
+        AddItem(enScoreType.Homework, "ผลทำการบ้าน")
+    End Sub
+End Class
