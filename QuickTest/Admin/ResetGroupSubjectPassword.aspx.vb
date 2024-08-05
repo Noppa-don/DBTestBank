2a1e40eda70fe64d9649044c95710b0a2304b38a
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 11:40:32 +0700

    Create ResetGroupSubjectPassword.aspx.vb

---
+++ QuickTest/Admin/ResetGroupSubjectPassword.aspx.vb
@@ -0,0 +1,13 @@
+﻿Public Class ResetGroupSubjectPassword
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Session("UserId") Is Nothing Then
+            Response.Redirect("~/LoginPage.aspx")
+        End If
+        If HttpContext.Current.Session("UserName").ToString().ToLower() <> "admin" Then
+            Response.Redirect("~/Default.aspx")
+        End If
+    End Sub
+
+End Class
