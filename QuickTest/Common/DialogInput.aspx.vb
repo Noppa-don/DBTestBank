6e2dc485cbf67a725a8cafce05e8df97383ba818
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:13:17 +0700

    Create DialogInput.aspx.vb

---
+++ QuickTest/Common/DialogInput.aspx.vb
@@ -0,0 +1,14 @@
+﻿Public Class DialogInput
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim Msg() = Request.QueryString("msg").Split(",")
+        Dim Title = Msg(0)
+        Dim Head = Msg(1)
+        Dim OldValue = Msg(2)
+        Me.Title = Title
+        lblHead.Text = Head
+        TxtData.Text = OldValue
+    End Sub
+
+End Class
