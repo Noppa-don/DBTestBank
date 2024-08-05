f8e3319b4b7cb4a09c8cbfdbb540da1e1d2989ed
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:07:19 +0700

    Create EnumPlayerType.vb

---
+++ QuickTest/BusinessTablet360/Enum/EnumPlayerType.vb
@@ -0,0 +1,13 @@
+﻿Public Enum EnPlayerType
+    Teacher = 1
+    Student = 2
+End Enum
+
+Public Class EnumPlayerType
+    Inherits EnumRegister
+
+    Public Sub New()
+        AddItem(EnPlayerType.Teacher, "ครู")
+        AddItem(EnPlayerType.Student, "นักเรียน")
+    End Sub
+End Class
