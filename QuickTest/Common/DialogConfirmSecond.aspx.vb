0c59453ba3590340da2557864d09291564e2da2b
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:13:07 +0700

    Create DialogConfirmSecond.aspx.vb

---
+++ QuickTest/Common/DialogConfirmSecond.aspx.vb
@@ -0,0 +1,8 @@
+﻿Public Class DialogConfirmSecond
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        PartMsg.InnerText = Request.QueryString("msg")
+    End Sub
+
+End Class
