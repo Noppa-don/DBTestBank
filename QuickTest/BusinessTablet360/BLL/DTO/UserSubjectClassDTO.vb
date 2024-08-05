d2fd945902d457a07dac0c78a076aec95ed1854c
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:03:26 +0700

    Create UserSubjectClassDTO.vb

---
+++ QuickTest/BusinessTablet360/BLL/DTO/UserSubjectClassDTO.vb
@@ -0,0 +1,13 @@
+﻿Public Class UserSubjectClassDTO
+
+    Public Property GUID As Guid?
+    Public Property SubjectId As Integer?
+    Public Property GroupSubjectShortName As String
+    Public Property SubjectNameDummy As String
+    Public Property Level As String
+    Public Property Level_Name As String
+    Public Property Level_Dummy As String
+    Public Property IsActive As Boolean
+    Public Property UserId As Guid?
+
+End Class
