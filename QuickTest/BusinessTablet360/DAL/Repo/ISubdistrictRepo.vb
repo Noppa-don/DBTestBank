67cae19d7d65a0078302b7bd8f02f0d3e5ebbd63
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:06:30 +0700

    Create ISubdistrictRepo.vb

---
+++ QuickTest/BusinessTablet360/DAL/Repo/ISubdistrictRepo.vb
@@ -0,0 +1,17 @@
+﻿Imports KnowledgeUtils.Database.DBFactory.LinqToSql(Of BusinessTablet360.DataClassesTablet360DataContext)
+Imports KnowledgeUtils.Database.DBFactory
+Imports KnowledgeUtils.Database
+
+Public Interface ISubdistrictRepo
+    Function GetSubdistrictByCrit(ByVal Ctx As DataClassesTablet360DataContext, ByVal Item As tblAmphur) As tblTambol()
+End Interface
+
+Public Class SubdistrictRepo
+    Implements ISubdistrictRepo
+
+    Public Function GetSubdistrictByCrit(ByVal Ctx As DataClassesTablet360DataContext, ByVal Item As tblAmphur) As tblTambol() Implements ISubdistrictRepo.GetSubdistrictByCrit
+        Return Ctx.tblTambols.Where(Function(r) r.AmphurId = Item.AmphurId).ToArray
+    End Function
+
+End Class
+
