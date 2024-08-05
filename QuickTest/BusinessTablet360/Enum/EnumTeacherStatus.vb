284e4f902c38a913d0f1b807b53ffe2b3532f7ce
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:07:55 +0700

    Create EnumTeacherStatus.vb

---
+++ QuickTest/BusinessTablet360/Enum/EnumTeacherStatus.vb
@@ -0,0 +1,14 @@
+﻿Public Enum EnTeacherStatus
+    Resign
+    Teach
+End Enum
+
+Public Class EnumTeacherStatus
+    Inherits EnumRegister
+
+    Public Sub New()
+        AddItem(EnStudentStatus.Resign, "ลาออก")
+        AddItem(EnStudentStatus.Study, "สอนอยู่")
+    End Sub
+
+End Class
