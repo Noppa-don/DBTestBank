c132a9af1636ffc106be98933cf3ddeea5d95229
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 11:37:13 +0700

    Create EmptySession.aspx.vb

---
+++ QuickTest/Activity/EmptySession.aspx.vb
@@ -0,0 +1,12 @@
+﻿Public Class EmptySession
+    Inherits System.Web.UI.Page
+    Public DVID As String
+    Public GroupName As String
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        DVID = Request.QueryString("DeviceId")
+
+    End Sub
+
+End Class
