cd421d881c0c3f21dab4ced91ade6ef315e515a5
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 11:40:02 +0700

    Create AdminPage.aspx.vb

---
+++ QuickTest/Admin/AdminPage.aspx.vb
@@ -0,0 +1,10 @@
+﻿Public Class AdminPage1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Session("UserId") = Nothing Then
+            Response.Redirect("~/LoginPage.aspx", False)
+        End If
+    End Sub
+
+End Class
