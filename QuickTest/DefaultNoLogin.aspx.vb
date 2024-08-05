a2b92935e8b12a992fb22df5414ab83d9be750b5
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:18:56 +0700

    Create DefaultNoLogin.aspx.vb

---
+++ QuickTest/DefaultNoLogin.aspx.vb
@@ -0,0 +1,10 @@
+﻿Public Class DefaultNoLogin
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        ClsLog.Record("PageLoad_DefaultNoLogin")
+    End Sub
+
+
+
+End Class
