c1ee1648b974aab7c0ae03cb9792bafe676ae6ab
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:10:59 +0700

    Create CommonTexts.vb

---
+++ QuickTest/Class/CommonTexts.vb
@@ -0,0 +1,20 @@
+﻿Friend NotInheritable Class CommonTexts
+    Public Const EditTestSetWarningText As String = """เมื่อทำการแก้ไขใดๆ จะทำการบันทึกอัตโนมัติให้ทันทีค่ะ<br />(ทั้งนี้หากเลือกวิชาออกหมดหรือเลือกข้อสอบออกหมด ข้อสอบชุดนี้จะโดนลบทิ้งอัตโนมัตินะคะ)"""
+
+
+    'ตัวชี้วัด
+    Public Const Indicators As String = "ตัวชี้วัด"
+    Public Const Course51 As String = "หลักสูตรแกนกลางฯ ปี ๒๕๕๑"
+
+    'KPA
+    Public Const KPA As String = "KPA"
+    Public Const K As String = "K - ด้านความรู้"
+    Public Const P As String = "P - ด้านทักษะกระบวนการ"
+    Public Const A As String = "A - ด้านคุณลักษณะ"
+
+    'แบบทดสอบระดับชาติ
+    Public Const NationalTest As String = "แบบทดสอบระดับชาติ"
+
+    'ระดับความยากง่าย
+    Public Const LevelOfTest As String = "ระดับความยากง่าย"
+End Class
