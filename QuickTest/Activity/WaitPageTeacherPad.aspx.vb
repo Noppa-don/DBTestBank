2a219ea5149e1440c701941f88c641f7395e6e34
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 11:39:13 +0700

    Create WaitPageTeacherPad.aspx.vb

---
+++ QuickTest/Activity/WaitPageTeacherPad.aspx.vb
@@ -0,0 +1,13 @@
+﻿Public Class WaitPageTeacherPad
+    Inherits System.Web.UI.Page
+    Public GroupName As String
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        GroupName = Session("selectedSession").ToString() 'GroupName
+
+        Dim ClsSelectedSession As New ClsSelectSession
+        ClsSelectedSession.checkCurrentPage(Session("UserId").ToString(), GroupName)
+    End Sub
+
+End Class
