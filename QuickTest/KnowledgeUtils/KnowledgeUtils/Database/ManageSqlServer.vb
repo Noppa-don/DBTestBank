af3cac93177c8c11f88fda91a9654d8eff5652bc
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 14:13:51 +0700

    Create ManageSqlServer.vb

---
+++ QuickTest/KnowledgeUtils/KnowledgeUtils/Database/ManageSqlServer.vb
@@ -0,0 +1,23 @@
+﻿
+Namespace Database.SqlServer
+
+    ''' <summary>
+    ''' คลาสช่วยจัดการ Sql Server
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Class ManageSqlServer
+
+        Public Shared Function ConvertDateTimeToString(ByVal DateSource As DateTime) As String
+            Dim D = DateSource.Year.ToString & "-" & DateSource.Month.ToString("00") & "-" & DateSource.Day.ToString("00")
+            Dim T = DateSource.Hour.ToString("00") & ":" & DateSource.Minute.ToString("00") & ":" & DateSource.Second.ToString("00") &
+                    "." & DateSource.Millisecond.ToString("000")
+
+            Return D & " " & T
+        End Function
+
+    End Class
+
+End Namespace
+
+
+
