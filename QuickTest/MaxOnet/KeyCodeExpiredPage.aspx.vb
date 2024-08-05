f5a05ca1ab7af92213651551c0ecb36aa32e3462
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 14:29:17 +0700

    Create KeyCodeExpiredPage.aspx.vb

---
+++ QuickTest/MaxOnet/KeyCodeExpiredPage.aspx.vb
@@ -0,0 +1,12 @@
+﻿Public Class KeyCodeExpiredPage
+    Inherits System.Web.UI.Page
+    Protected TokenId As String
+    Protected DeviceId As String
+    Protected SubjectsIdStr As String
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        TokenId = Request.QueryString("TokenId")
+        DeviceId = Request.QueryString("DeviceId")
+        SubjectsIdStr = Request.QueryString("SubjectsIdStr")
+    End Sub
+
+End Class
