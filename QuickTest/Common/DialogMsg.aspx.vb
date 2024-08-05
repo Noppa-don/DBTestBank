3bbe21af0e1c036bc1adeea3c84bb8f09e30c791
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:13:27 +0700

    Create DialogMsg.aspx.vb

---
+++ QuickTest/Common/DialogMsg.aspx.vb
@@ -0,0 +1,9 @@
+﻿
+Public Class DialogMsg
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        PartMsg.InnerText = Request.QueryString("msg")
+    End Sub
+
+End Class
