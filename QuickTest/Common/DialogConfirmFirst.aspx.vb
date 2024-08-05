1811b3a974ce9ca788a10eef36d40e13c5603188
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:12:57 +0700

    Create DialogConfirmFirst.aspx.vb

---
+++ QuickTest/Common/DialogConfirmFirst.aspx.vb
@@ -0,0 +1,11 @@
+﻿Public Class DialogConfirmFirst
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim Msg = Request.QueryString("msg")
+        Dim ListMsg() = Msg.Split(",")
+        PartMsg.InnerText = ListMsg(0)
+        HfMsg2.Value = ListMsg(1)
+    End Sub
+
+End Class
