a232ee34d6b97621e0d98e044eecde0b244e6ef9
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:07:03 +0700

    Create EnumMobileAccessType.vb

---
+++ QuickTest/BusinessTablet360/Enum/EnumMobileAccessType.vb
@@ -0,0 +1,34 @@
+﻿
+Public Enum EnMobileAccessType
+    Director
+    It
+    HeadThai
+    HeadArt
+    HeadWork
+    HeadHealth
+    HeadForeign
+    HeadSocial
+    HeadScience
+    HeadMath
+    HeadOther
+End Enum
+
+Public Class EnumMobileAccessType
+    Inherits EnumRegister
+
+    Public Sub New()
+        AddItem(EnMobileAccessType.Director, "ผอ")
+        AddItem(EnMobileAccessType.It, "it")
+        AddItem(EnMobileAccessType.HeadThai, "กลุ่มสาระการเรียนรู้ภาษาไทย")
+        AddItem(EnMobileAccessType.HeadArt, "กลุ่่มสาระการเรียนรู้ศิลปะ")
+        AddItem(EnMobileAccessType.HeadWork, "กลุ่มสาระการเรียนรู้การงานอาชีพและเทคโนโลยี")
+        AddItem(EnMobileAccessType.HeadHealth, "กลุ่มสาระการเรียนรู้สุขศึกษาและพละศึกษา")
+        AddItem(EnMobileAccessType.HeadForeign, "กลุ่มสาระการเรียนรู้ภาษาต่างประเทศ")
+        AddItem(EnMobileAccessType.HeadSocial, "กลุ่มสาระการเรียนรู้สังคมศึกษาศาสนาและวัฒนธรรม")
+        AddItem(EnMobileAccessType.HeadScience, "กลุ่มสาระการเรียนรู้วิทยาศาสตร์")
+        AddItem(EnMobileAccessType.HeadMath, "กลุ่มสาระการเรียนรู้คณิตศาสตร์")
+        AddItem(EnMobileAccessType.HeadOther, "อื่น")
+
+    End Sub
+
+End Class
