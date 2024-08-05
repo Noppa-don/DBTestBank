4a34389151738407d087431a41c362197689a831
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 12:09:40 +0700

    Create ClsAllAnonymous.vb

---
+++ QuickTest/Class/ClsAllAnonymous.vb
@@ -0,0 +1,16 @@
+﻿Public Class ClsAllAnonymous
+
+End Class
+
+<Serializable()> _
+Public Class Selectedsubjects
+    Public KeyId As Object
+    Public ClassId As String
+    Public ClassName As String
+    Public SubjectName As String
+    Public SubjectId As String
+
+    Public Sub New()
+
+    End Sub
+End Class
