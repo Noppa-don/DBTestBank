104868c67d0eff11aac86294e58ce6f697b4a57f
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-05 11:40:22 +0700

    Create ResetAdminPassword.aspx.vb

---
+++ QuickTest/Admin/ResetAdminPassword.aspx.vb
@@ -0,0 +1,10 @@
+﻿Public Class ResetAdminPassword
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Service.ClsSystem.CheckIsLocalhost() = False Then
+            Response.Redirect("~/Loginpage.aspx")
+        End If
+    End Sub
+
+End Class
