02068208b429dc97621edf5de3f2cb3786566588
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-06 13:43:34 +0700

    Update CreateEvaluationIndex.aspx.vb

--- QuickTest/TestSet/CreateEvaluationIndex.aspx.vb
+++ QuickTest/TestSet/CreateEvaluationIndex.aspx.vb
@@ -11,7 +11,6 @@ Public Class CreateEvaluationIndex
             Response.Redirect("~/LoginPage.aspx", False)
         Else
             lblWarn.Visible = False
-
             If Not Page.IsPostBack Then
                 BindDataOnLoad()
             End If
@@ -31,7 +30,7 @@ Public Class CreateEvaluationIndex
         dt1 = useCls.getdata(sql1)
 
         If dt1.Rows.Count > 0 Then
-          
+
             For i = 0 To dt1.Rows.Count - 1
                 ListIndexName.Items.Add(New RadListBoxItem With {.Text = dt1.Rows(i)("EI_Code"), .Value = dt1.Rows(i)("EI_Id").ToString})
             Next
@@ -81,7 +80,6 @@ Public Class CreateEvaluationIndex
     Private Sub ListIndexName_SelectedIndexChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles ListIndexName.SelectedIndexChanged
 
         Session("IndexNameID") = ListIndexName.SelectedValue.ToString
-
         ListIndexGroupName.Items.Clear()
 
         Dim sql2 As String = "select EI_Id,EI_Code from tblEvaluationIndexNew where Parent_Id = '" & ListIndexName.SelectedValue.ToString & "' And IsActive = 1 order by EI_Code;"
@@ -109,7 +107,6 @@ Public Class CreateEvaluationIndex
             ListIndexItem.SelectedIndex = 0
             Session("IndexItemId") = dt2.Rows(0)("EI_Id").ToString
         End If
-
     End Sub
 
     Private Sub ListIndexGroupName_SelectedIndexChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles ListIndexGroupName.SelectedIndexChanged
@@ -129,7 +126,6 @@ Public Class CreateEvaluationIndex
             ListIndexItem.SelectedIndex = 0
             Session("IndexItemId") = dt2.Rows(0)("EI_Id").ToString
         End If
-
     End Sub
 
     Private Sub ListIndexItem_SelectedIndexChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles ListIndexItem.SelectedIndexChanged
@@ -195,9 +191,9 @@ Public Class CreateEvaluationIndex
             HttpContext.Current.Response.Write("โปรดเลือกชื่อดัชนี หรือ สร้างดัชนีก่อน")
         Else
             Dim IndexNameId As String = HttpContext.Current.Session("IndexNameID")
-            Dim sql As String '= "Insert Into tblEvaluationIndexGroup(EI_Id,EIG_NeedSingleChoice,EIG_Name) values('" & IndexNameId.ToString & "','" & NeedSingleChoice & "','" & dataNewIndexGroupName & "')"
+            Dim sql As String
             sql = "Insert into tblEvaluationIndexNew SELECT '" & IndexGroupId.ToString & "','" & dataNewIndexGroupName & "',null,
-                    '" & IndexNameId.ToString & "',2,'0',1,dbo.GetThaiDate(),null,max(EI_Position) + 1,null,null 
+                    '" & IndexNameId.ToString & "',1,'0',1,dbo.GetThaiDate(),null,max(EI_Position) + 1,null,null 
                     FROM tblEvaluationIndexNew where Parent_Id = '" & IndexNameId.ToString & "';"
             Try
                 Usecls.Execute(sql)
@@ -247,6 +243,180 @@ Public Class CreateEvaluationIndex
     End Function
 
 #End Region
+    '20240802 แก้ไขและลบตัวชี้วัด
+#Region "Edit"
+    <WebMethod()>
+    Public Shared Function UpdateNewIndexName(ByVal dataNewIndexName As String) As Boolean
 
+        Dim selected As String = HttpContext.Current.Session("IndexNameID")
+
+        Dim IndexId As String = selected.ToString
+        Dim UseCls As New ClassConnectSql
+
+        Dim sql As String
+        sql = "insert into tblApproveEvaluationIndex select newid(),1,ei_Id,1,0,EI_Code,
+               '" & dataNewIndexName & "','" & HttpContext.Current.Session("UserId").ToString & "',null,1,getdate() 
+               from tblEvaluationIndexNew where ei_Id = '" & IndexId & "';
+               Update  tblevaluationIndexNew set Ei_Code = '" & dataNewIndexName & "',LastUpdate = Getdate() where EI_Id = '" & IndexId & "'; "
+        Try
+            UseCls.Execute(sql)
+            HttpContext.Current.Session("IndexNameID") = IndexId.ToString
+            Dim StrLog As String = "แก้ไขดัชนี " & dataNewIndexName & " Id =" & IndexId.ToString
+            Log.Record(Log.LogType.ManageExam, UseCls.CleanString(StrLog), True)
+            Return 1
+        Catch ex As Exception When Not TypeOf ex Is Threading.ThreadAbortException
+            Log.Record(Log.LogType.ManageExam, UseCls.CleanString(ex.ToString), True)
+            Return 0
+        End Try
+    End Function
+    <WebMethod()>
+    Public Shared Function UpdateIndexGroup(ByVal dataNewIndexGroupName As String) As Boolean
+
+        Dim IndexGroupId As String = HttpContext.Current.Session("IndexGroupID")
+        Dim Usecls As New ClassConnectSql
+
+        If IndexGroupId = "" Then
+            HttpContext.Current.Response.Write("โปรดเลือกชื่อดัชนี หรือ สร้างดัชนีก่อน")
+        Else
+            Dim sql As String
+            sql = "insert into tblApproveEvaluationIndex select newid(),2,ei_Id,1,0,EI_Code,
+                   '" & dataNewIndexGroupName & "','" & HttpContext.Current.Session("UserId").ToString & "',null,1,getdate() 
+                   from tblEvaluationIndexNew where ei_Id = '" & IndexGroupId & "';
+                   Update  tblevaluationIndexNew set Ei_Code = '" & dataNewIndexGroupName & "',LastUpdate = Getdate() where EI_Id = '" & IndexGroupId & "';"
+            Try
+                Usecls.Execute(sql)
+                HttpContext.Current.Session("IndexGroupID") = IndexGroupId.ToString
+                Dim StrLog As String = "แก้ไขกลุ่มดัชนี " & dataNewIndexGroupName & " Id =" & IndexGroupId.ToString
+                Log.Record(Log.LogType.ManageExam, Usecls.CleanString(StrLog), True)
+                Return 1
+            Catch ex As Exception When Not TypeOf ex Is Threading.ThreadAbortException
+                Log.Record(Log.LogType.ManageExam, Usecls.CleanString(ex.ToString), True)
+                Return 0
+            End Try
+        End If
+    End Function
+    <WebMethod()>
+    Public Shared Function UpdateIndexItem(ByVal dataNewIndexItemName As String) As Boolean
+        Dim IndexItemId As String = HttpContext.Current.Session("IndexItemId").ToString
+        Dim UseCls As New ClassConnectSql
+
+        If IndexItemId Is Nothing Then
+            HttpContext.Current.Response.Write("โปรดเลือกกลุ่มดัชนีก่อน หรือ สร้างกลุ่มดัชนีก่อน")
+        Else
+            Dim sql As String
+            sql = "insert into tblApproveEvaluationIndex select newid(),3,ei_Id,1,0,EI_Code,
+                   '" & dataNewIndexItemName & "','" & HttpContext.Current.Session("UserId").ToString & "',null,1,getdate() 
+                   from tblEvaluationIndexNew where ei_Id = '" & IndexItemId & "';
+                   Update  tblevaluationIndexNew set Ei_Code = '" & dataNewIndexItemName & "',LastUpdate = Getdate() where EI_Id = '" & IndexItemId & "';"
+
+            Try
+                UseCls.Execute(sql)
+                HttpContext.Current.Session("IndexItemId") = IndexItemId.ToString
+                Dim StrLog As String = "แก้ไขตัวชี้วัด " & dataNewIndexItemName & " Id =" & IndexItemId.ToString
+                Log.Record(Log.LogType.ManageExam, UseCls.CleanString(StrLog), True)
+                Return 1
+            Catch ex As Exception When Not TypeOf ex Is Threading.ThreadAbortException
+                Log.Record(Log.LogType.ManageExam, UseCls.CleanString(ex.ToString), True)
+                Return 0
+            End Try
+        End If
+    End Function
+#End Region
+
+#Region "Delete"
+
+    Protected Sub btnDeleteIndexName_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnDeleteIndexName.Click
+
+        Dim IndexnameSelected As String = ListIndexName.SelectedValue.ToString
+        Dim IndexText As String = ListIndexName.SelectedItem.ToString
+
+        If IndexnameSelected <> "" Then
+
+            Dim sql As String
+
+            sql = "Update tblevaluationIndexNew set IsActive = 0 , Lastupdate = getdate() where ei_ID = '" & IndexnameSelected & "';
+                    insert into tblApproveEvaluationIndex select newid(),1,ei_Id,2,0,EI_Code,null ,'" & HttpContext.Current.Session("UserId").ToString & "',null,1,getdate() 
+                    from tblEvaluationIndexNew where ei_Id = '" & IndexnameSelected & "';"
+
+            Try
+                useCls.Execute(sql.ToString)
+            Catch ex As Exception When Not TypeOf ex Is Threading.ThreadAbortException
+                Elmah.ErrorSignal.FromCurrentContext().Raise(ex)
+            End Try
+
+            Dim StrLog As String = "ลบดัชนี " & IndexText & " Id =" & IndexnameSelected.ToString
+            Log.Record(Log.LogType.ManageExam, useCls.CleanString(StrLog), True)
+
+            Session("IndexNameID") = Nothing
+            Response.Redirect("CreateEvaluationIndex.aspx")
+        Else
+            lblWarn.Visible = True
+            lblWarn.Text = "ต้องเลือกดัชนีก่อนจึงจะสามารถลบได้"
+        End If
+
+    End Sub
+
+    Protected Sub btnDeleteIndexGroupName_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnDeleteIndexGroupName.Click
+
+
+        Dim IndexGroupSelected As String = ListIndexGroupName.SelectedValue.ToString
+        Dim IndexGroupText As String = ListIndexGroupName.SelectedItem.ToString
+        If IndexGroupSelected <> "" Then
+
+            Dim sql As String
+            sql = "Update tblevaluationIndexNew set IsActive = 0 , Lastupdate = getdate() where ei_ID = '" & IndexGroupSelected & "';
+                    insert into tblApproveEvaluationIndex select newid(),2,ei_Id,2,0,EI_Code,null ,'" & HttpContext.Current.Session("UserId").ToString & "',null,1,getdate() 
+                    from tblEvaluationIndexNew where ei_Id = '" & IndexGroupSelected & "';"
+
+            Try
+                useCls.Execute(sql.ToString)
+            Catch ex As Exception When Not TypeOf ex Is Threading.ThreadAbortException
+                Elmah.ErrorSignal.FromCurrentContext().Raise(ex)
+            End Try
+            Dim StrLog As String = "ลบกลุ่มดัชนี " & IndexGroupText & " Id =" & IndexGroupSelected.ToString
+            Log.Record(Log.LogType.ManageExam, useCls.CleanString(StrLog), True)
+            Session("IndexGroupID") = Nothing
+            Response.Redirect("CreateEvaluationIndex.aspx")
+
+        Else
+            lblWarn.Visible = True
+            lblWarn.Text = "ต้องเลือกกลุ่มดัชนีก่อนจึงจะสามารถลบได้"
+        End If
+
+
+    End Sub
+
+    Private Sub btnDeleteIndexItem_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnDeleteIndexItem.Click
+
+        Dim IndexItemSelected As String = ListIndexItem.SelectedValue.ToString
+        Dim IndexItemText As String = ListIndexItem.SelectedItem.ToString
+        If IndexItemSelected <> "" Then
+
+            Dim sql As String
+            sql = "Update tblevaluationIndexNew set IsActive = 0 , Lastupdate = getdate() where ei_ID = '" & IndexItemSelected & "';
+                    insert into tblApproveEvaluationIndex select newid(),3,ei_Id,2,0,EI_Code,null ,'" & HttpContext.Current.Session("UserId").ToString & "',null,1,getdate() 
+                   from tblEvaluationIndexNew where ei_Id = '" & IndexItemSelected & "';"
+
+            Try
+                useCls.Execute(sql.ToString)
+            Catch ex As Exception When Not TypeOf ex Is Threading.ThreadAbortException
+                Elmah.ErrorSignal.FromCurrentContext().Raise(ex)
+                lblWarn.Text = ex.ToString
+            End Try
+            Dim StrLog As String = "ลบดัชนี " & IndexItemText & " Id =" & IndexItemSelected.ToString
+            Log.Record(Log.LogType.ManageExam, useCls.CleanString(StrLog), True)
+            Session("IndexItemId") = Nothing
+            Response.Redirect("CreateEvaluationIndex.aspx")
+
+        Else
+            lblWarn.Visible = True
+            lblWarn.Text = "ต้องเลือกตัวชี้วัดก่อนจึงจะสามารถลบได้"
+        End If
+
+
+
+    End Sub
+
+#End Region
 
 End Class
