f2a0b39163e62d3c5dcd94df3a554ff89ce0db3b
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-06 10:08:36 +0700

    Create EnumRecordStatus.vb

---
+++ BusinessTablet360/Enum/EnumRecordStatus.vb
@@ -0,0 +1,19 @@
+﻿Public Enum EnRecordStatus
+    Import = 1
+    Modify = 2
+    Skip = 3
+    ChangeStatus = 5
+    Delete = 6
+End Enum
+
+Public Class EnumRecordStatus
+    Inherits EnumRegister
+
+    Public Sub New()
+        AddItem(EnRecordStatus.Import, "นำเข้า")
+        AddItem(EnRecordStatus.Modify, "ปรับปรุงข้อมูล")
+        AddItem(EnRecordStatus.Skip, "ข้าม")
+        AddItem(EnRecordStatus.ChangeStatus, "ปรับสถานะ")
+        AddItem(EnRecordStatus.Delete, "ลาออก")
+    End Sub
+End Class
