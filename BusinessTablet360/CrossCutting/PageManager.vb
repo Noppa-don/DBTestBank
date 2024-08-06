08df79e1b74918b6b7fd99ed50aab192979db602
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-06 10:06:40 +0700

    Create PageManager.vb

---
+++ BusinessTablet360/CrossCutting/PageManager.vb
@@ -0,0 +1,8 @@
+﻿
+Public MustInherit Class PageManager
+
+    Public MustOverride Sub DisplayUIDynamic(ByVal Status As EnStatusUI)
+    Public MustOverride Function ValidatePage(ByVal Status As EnStatusUI) As Boolean
+    Public MustOverride Sub BindGrid(Optional ByVal PageIndex As Integer = 0)
+
+End Class
