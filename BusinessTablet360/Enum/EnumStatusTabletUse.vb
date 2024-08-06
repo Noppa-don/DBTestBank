e8cb4d5b6de142c74437ac39c70cfae1ca937355
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-06 10:08:46 +0700

    Create EnumStatusTabletUse.vb

---
+++ BusinessTablet360/Enum/EnumStatusTabletUse.vb
@@ -0,0 +1,21 @@
+﻿
+Public Enum EnStatusTabletUse
+    Always
+    Often
+    Rarely
+    Less
+    None
+End Enum
+
+Public Class EnumStatusTabletUse
+    Inherits EnumRegister
+
+    Public Sub New()
+        AddItem(EnStatusTabletUse.Always, "สม่ำเสมอ")
+        AddItem(EnStatusTabletUse.Often, "บ่อยครั้ง")
+        AddItem(EnStatusTabletUse.Rarely, "นานๆครั้ง")
+        AddItem(EnStatusTabletUse.Less, "แทบจะไม่")
+        AddItem(EnStatusTabletUse.None, "ไม่มีเลย")
+    End Sub
+
+End Class
