b877f02329f14c2a6149efebd2dea8ddcc21b4e1
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-06 10:07:59 +0700

    Create EnumDashBoard.vb

---
+++ BusinessTablet360/Enum/EnumDashBoard.vb
@@ -0,0 +1,23 @@
+﻿Public Enum EnumDashBoardType
+    Quiz = 1
+    Homework = 2
+    Practice = 3
+    PrintTestset = 4
+    SetUp = 5
+    PracticeFromComputer = 6
+End Enum
+
+
+Public Class EnumDashBoard
+    Inherits EnumRegister
+
+    Public Sub New()
+        AddItem(EnumDashBoardType.Quiz, "ควิซ")
+        AddItem(EnumDashBoardType.Homework, "การบ้าน")
+        AddItem(EnumDashBoardType.Practice, "ฝึกฝน")
+        AddItem(EnumDashBoardType.PrintTestset, "พิมพ์ข้อสอบ/Genword")
+        AddItem(EnumDashBoardType.SetUp, "จัดชุดใหม่")
+        AddItem(EnumDashBoardType.PracticeFromComputer, "ฝึกฝนจากคอมพิวเตอร์")
+    End Sub
+
+End Class
