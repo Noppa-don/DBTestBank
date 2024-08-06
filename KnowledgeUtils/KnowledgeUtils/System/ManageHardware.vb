be9cf91005de93c515f8484dbebe02c8d67b0d63
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-06 09:34:28 +0700

    Create ManageHardware.vb

---
+++ KnowledgeUtils/KnowledgeUtils/System/ManageHardware.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Management
+
+Namespace System
+
+    Public Class ManageHardware
+
+        Public Shared Function GetCpuId() As String
+            Dim Query As New SelectQuery("Win32_processor")
+            Dim Search As New ManagementObjectSearcher(Query)
+            Dim id As String = ""
+            For Each Info As ManagementObject In Search.Get()
+                id = Info("processorid").ToString
+            Next
+            Return id
+        End Function
+
+    End Class
+
+End Namespace
+
+
