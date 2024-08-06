5ca229209550dfb43b9918a5636a45285a1c7b0c
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-06 10:04:17 +0700

    Create SubjectDTO.vb

---
+++ BusinessTablet360/BLL/DTO/SubjectDTO.vb
@@ -0,0 +1,24 @@
+﻿Public Class SubjectDTO
+
+    Public Property School_Code As String
+    Public Property Subject_Id As Guid?
+    Public Property Subject_Code As String
+    Public Property Subject_Name As String
+    Public Property ST_Id As Guid?
+    Public Property Subject_IsActive As Boolean?
+
+    'Public Property GroupSubject_Id As Guid
+    'Public Property GroupSubject_ShortName As String
+    'Public Property GroupSubject_Name As String
+
+    'Public Property SubjectId As Integer
+    'Public Property SubjectName As String
+    'Public Property SubjectNameDummy As String
+
+    Public Property GroupSubject_Id As Guid?
+    Public Property SubjectNumber As Integer
+    Public Property SubjectNameDummy As String
+    Public Property GroupSubjectShortName As String
+
+
+End Class
