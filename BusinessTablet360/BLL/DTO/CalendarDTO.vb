5ab5cf18ac62e3b2852dd8ef8b01874c5da56f13
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-06 10:03:41 +0700

    Create CalendarDTO.vb

---
+++ BusinessTablet360/BLL/DTO/CalendarDTO.vb
@@ -0,0 +1,16 @@
+﻿
+Public Class CalendarDTO
+
+    Public Property School_Code As String
+    Public Property Calendar_Year As String
+    Public Property Calendar_Name As String
+    Public Property Calendar_FromDate As Date?
+    Public Property Calendar_FromDateString As String
+    Public Property Calendar_ToDate As Date?
+    Public Property Calendar_ToDateString As String
+    Public Property Calendar_Type As Byte?
+    Public Property CurrentMonth As Integer?
+    Public Property CurrentYear As Integer?
+    Public Property Calendar_Id As Guid?
+
+End Class
