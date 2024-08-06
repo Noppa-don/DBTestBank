5f46faaf923fedcb3f770d7adcffc100e2f18ef8
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-06 10:09:09 +0700

    Create EnumUseTabletType.vb

---
+++ BusinessTablet360/Enum/EnumUseTabletType.vb
@@ -0,0 +1,20 @@
+﻿Public Enum EnUseTabletType
+    Other
+    ReadBook
+    DoHomework
+    PlayInternet
+    PlayGame
+End Enum
+
+Public Class EnumUseTabletType
+    Inherits EnumRegister
+
+    Public Sub New()
+        AddItem(EnUseTabletType.Other, "อื่นๆ")
+        AddItem(EnUseTabletType.ReadBook, "อ่านบทเรียน")
+        AddItem(EnUseTabletType.DoHomework, "ทำการบ้าน")
+        AddItem(EnUseTabletType.PlayInternet, "ใช้อินเทอร์เน็ต")
+        AddItem(EnUseTabletType.PlayGame, "เล่นเกมส์")
+    End Sub
+
+End Class
