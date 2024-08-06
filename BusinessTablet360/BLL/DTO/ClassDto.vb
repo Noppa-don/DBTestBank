2ac7947e7923bf3181d3dd4923b5569900679028
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-06 10:03:48 +0700

    Create ClassDto.vb

---
+++ BusinessTablet360/BLL/DTO/ClassDto.vb
@@ -0,0 +1,12 @@
+﻿Public Class ClassDto
+
+    Public Property Class_Name As String
+    Public Property Room_Name As String
+    Public Property Room_Id As Guid
+
+    Public Property Level_Id As Guid
+    Public Property Level As String
+    Public Property Level_Name As String
+    Public Property Level_Dummy As String
+
+End Class
