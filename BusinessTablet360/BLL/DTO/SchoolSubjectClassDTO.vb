027dda6fb1a835bd796835809fbba3510046d7b7
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-06 10:04:11 +0700

    Create SchoolSubjectClassDTO.vb

---
+++ BusinessTablet360/BLL/DTO/SchoolSubjectClassDTO.vb
@@ -0,0 +1,7 @@
+﻿Public Class SchoolSubjectClassDTO
+    Public Property SSCId As Guid
+    Public Property SchoolCode As String
+    Public Property GroupSubjectId As Guid
+    Public Property LevelId As Guid
+    Public Property IsActive As Boolean
+End Class
